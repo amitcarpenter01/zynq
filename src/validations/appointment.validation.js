1c225826a1652af53412403c6c403bfbae08277b
Gourav-ctinfotech <Gouravvaidya.github@gmail.com>
2025-07-03 18:36:31 +0530

    fixed date validation

--- src/validations/appointment.validation.js
+++ src/validations/appointment.validation.js
@@ -2,11 +2,12 @@ import joi from "joi";
 import {
   stringValidation,
   idValidation,
+  dateValidation,
 } from "../utils/joi.util.js";
 
 export const rescheduleAppointmentSchema = joi.object({
   appointment_id: idValidation,
   doctor_id: idValidation,
-  start_time: stringValidation,
-  end_time: stringValidation
+  start_time: dateValidation,
+  end_time: dateValidation
 });
