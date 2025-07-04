c389c3d1e3b0495b4e0ddfa7a4318a0d889b5bfb
YashrajCti <yashrajmandloi.github@gmail.com>
2025-07-04 15:40:09 +0530

    changes

--- src/services/solo_multer.js
+++ src/services/solo_multer.js
@@ -7,15 +7,40 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Create upload folder if it doesn't exist
-const uploadDir = path.join(__dirname, '../uploads/solo_doctor');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
+// const uploadDir = path.join(__dirname, '../uploads/solo_doctor');
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadDir);
+//   },
+//   filename: function (req, file, cb) {
+//     const sanitizedName = file.originalname.replace(/\s+/g, '_');
+//     cb(null, `${Date.now()}-${sanitizedName}`);
+//   },
+// });
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir);
+    let targetDir = '';
+
+    if (file.fieldname === 'profile') {
+      targetDir = path.join(__dirname, '../uploads/doctor/profile_images');
+    } else if (file.fieldname === 'logo') {
+      targetDir = path.join(__dirname, '../uploads/clinic/logo');
+    } else {
+      return cb(new Error('Invalid file field name'), null);
+    }
+
+    // Ensure the target directory exists
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir, { recursive: true });
+    }
+
+    cb(null, targetDir);
   },
+
   filename: function (req, file, cb) {
     const sanitizedName = file.originalname.replace(/\s+/g, '_');
     cb(null, `${Date.now()}-${sanitizedName}`);
