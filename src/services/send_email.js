6b5f7cf9fcff9a332d8e0029b2bb57fd4ffcd65c
Gourav-ctinfotech <Gouravvaidya.github@gmail.com>
2025-08-18 17:05:03 +0530

    dashboard changes for solo doctor management

--- src/services/send_email.js
+++ src/services/send_email.js
@@ -28,6 +28,5 @@ export const sendEmail = async ({ to, subject, html }) => {
         console.log(`Email sent to ${to}`);
     } catch (error) {
         console.error('Error sending email:', error);
-        throw new Error('Error sending email');
     }
 };
