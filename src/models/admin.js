cceb1b03a193e56cca3096b2cd0192b737d26780
Gourav-ctinfotech <Gouravvaidya.github@gmail.com>
2025-08-22 12:16:02 +0530

    Implemented logic to filter paid appointments

--- src/models/admin.js
+++ src/models/admin.js
@@ -788,7 +788,7 @@ export const getAdminBookedAppointmentsModel = async () => {
         LEFT JOIN tbl_zqnq_users zu ON d.zynq_user_id = zu.id
         LEFT JOIN tbl_clinics c ON c.clinic_id = a.clinic_id
         LEFT JOIN tbl_users u ON u.user_id = a.user_id
-        WHERE a.save_type = 'booked' AND a.total_price > 0
+        WHERE a.save_type = 'booked' AND a.total_price > 0 And a.payment_status = 'paid'
         ORDER BY a.created_at DESC;
     `;
 
