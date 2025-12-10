80bcff205801524a121a7ab635ada4f70b37e0d6
Gourav-ctinfotech <Gouravvaidya.github@gmail.com>
2025-12-10 20:57:48 +0530

    Patient Record Get By Id

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -351,10 +351,10 @@ export const getAppointmentsByRole = async (id, role) => {
     const values = [];
 
     if (role === 'DOCTOR' || role === 'SOLO_DOCTOR') {
-        whereClause = 'a.doctor_id = ?';
+        whereClause = "a.doctor_id = ?  AND a.payment_status != 'unpaid'";
         values.push(id);
     } else if (role === 'CLINIC') {
-        whereClause = 'a.clinic_id = ?';
+        whereClause = "a.clinic_id = ?  AND a.payment_status != 'unpaid'";
         values.push(id);
     } else {
         throw new Error('Invalid role provided');
