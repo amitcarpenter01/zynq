fdd8f248d5cffa9528b6a4f476dad2b5e945473e
Gourav-ctinfotech <Gouravvaidya.github@gmail.com>
2025-12-22 17:39:37 +0530

    removed

--- app.js
+++ app.js
@@ -34,13 +34,6 @@ export const gemini = process.env.GEMINI_API_KEY ? new GoogleGenAI({}) : null;
 
 // --------------------- MIDDLEWARE ---------------------
 
-router.post(
-  "/stripe/webhook",
-  bodyParser.raw({ type: "application/json" }), // Stripe requires raw body
-  handleStripeWebhook
-);
-
-
 
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
